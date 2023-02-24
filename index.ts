cebec1a42b3d47f2dd1956db6b9cb7e1fe79d0fb
Tatsuro Baba <harakirisoul@gmail.com>
2023-02-24 16:26:13 +0900

    Make the first endpoint of the app

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (_req: Request, res: Response) => {
+  return res.status(200).send({
+    message: 'Hello, world!',
+  });
+});
+
+try {
+  app.listen(PORT, () => console.log(`dev server running at: http://localhost:${PORT}/`));
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message);
+  }
+}
